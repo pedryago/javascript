304d65bd2f853ae86bdc39d0fb59915fa221c906
pedryago <155452353+pedryago@users.noreply.github.com>
2024-01-23 21:43:41 -0300

    att ex014

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,1 +1,19 @@
-alert('Olá!')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 11
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'ex014/imagens/manha-pq.png'
+        document.body.style.background = '#BBE3FD'
+    } else if (hora >= 12 && hora < 18){
+        img.src = 'ex014/imagens/tarde-pq.png'
+        document.body.style.background = '#EA903A'
+    } else {
+        img.src = 'ex014/imagens/noite-pq.png'
+        document.body.style.background = '#011142'
+    }
+}
+
